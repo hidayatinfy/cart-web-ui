721ad8cd63e85bcbefcdcc13716f08ba53386638
Tushar Dimble <dimbletushar7004@gmail.com>
2020-08-09 19:09:48 +0530

    Header code added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,6 +16,59 @@ function cartData(){
             price: 430
         },
         {
+            image: '2.jpeg',
+            name: 'cake2',
+            price: 270
+        },
+        {
+            image: '1.jpeg',
+            name: 'cake3',
+            price: 320
+        },
+    ];
+    let myCartLength = myCart.length;
+    
+    for(let i=0; i<myCart.length;i++){
+        // Main Div
+        let iDiv = document.createElement('div');
+        iDiv.className = 'cart-item d-flex justify-content-between text-capitalize my-3';
+        document.getElementById('mycartdata').appendChild(iDiv);
+        
+        // For Image
+        let imgDiv = document.createElement('IMG');
+        imgDiv.className = 'img-fluid rounded-circle';
+        imgDiv.setAttribute("src", "assets/images/"+myCart[i]['image']);
+        iDiv.appendChild(imgDiv);
+
+        // Name Div
+        let iNamePriceDiv = document.createElement('item-text');
+        iNamePriceDiv.className = 'cart-item d-flex justify-content-between text-capitalize my-3';
+        iDiv.appendChild(iNamePriceDiv);
+
+        // Paragraph Tag
+        let sName = document.createElement('p');
+        sName.className = 'font-weight-bold mb-0';
+        sName.innerHTML = myCart[i]['name'];
+        iDiv.appendChild(sName);
+
+        // Span Price
+        let sSpan = document.createElement('span');
+        sSpan.className = 'cart-item-price';
+        sSpan.innerHTML = myCart[i]['price'];
+        iDiv.appendChild(sSpan);
+
+    }
+        
+}
+
+function cartData1(){
+    let myCart = [
+        {
+            image: '1.jpeg',
+            name: 'cake1',
+            price: 430
+        },
+        {
             image: 'cake-2.jpeg',
             name: 'cake2',
             price: 270
